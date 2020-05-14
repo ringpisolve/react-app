ededf6e17b2d82dfbc34ff1df210b0b177311ddf
ringpisolve@gmail.com <ringpisolve2019>
2020-05-14 09:34:27 +0530

    Updates

---
+++ precache-manifest.95316470a6e7cb87ad76bee73dda0931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3282c1efc9bfdcc60ca363a54fed7eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "539565b4828ea74b5134",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cadd30713a59146d449b",
+    "url": "/static/js/2.bd7fde5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd7fde5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539565b4828ea74b5134",
+    "url": "/static/js/main.e3682fd5.chunk.js"
+  },
+  {
+    "revision": "346e35f94ebf692dc860",
+    "url": "/static/js/runtime-main.6cf87bbc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
