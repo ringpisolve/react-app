49bd3f66d3c3a4d2c67e5d4563c7923bf52136a3
ringpisolve@gmail.com <ringpisolve2019>
2020-05-14 08:20:22 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>RING PI</h1>
+      <h5>--we build your ideas</h5>
     </div>
   );
 }
